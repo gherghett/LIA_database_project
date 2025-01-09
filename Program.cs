5fee7d67da25f3ae6b970d0460d43cefcc4f13b9
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-09 13:26:17 +0100

    make first main menu

--- Program.cs
+++ Program.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using InputHandler;
+using Microsoft.EntityFrameworkCore;
 using Models.Entities;
 
 Console.WriteLine("Hello, Krister!");
+    
+MenuBuilder.CreateMenu("Välkommen! Main Menu för -\\'LIA DB PROGRAMMET'/-")
+    .AddScreen("See överblick", Overview)
+    .AddQuit("Avsluta programmet")
+    .Enter();
 
-
-using (var context = new Context())
+void Overview() 
 {
-    var c = context.Companies.Where(c => c.Id == 1).Include(c => c.ContactPersons).Single();
-    Console.WriteLine(c.Name);
+    using (var context = new Context())
+    {
+        var companies = context.Companies.ToList();
+        foreach( var company in companies)
+        {
+            Console.WriteLine(company.Name);
+        }
+    }
 }
 
