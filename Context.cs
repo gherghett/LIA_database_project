fc7471a79833a6d2c7a4d69f26ed86d57918f685
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-09 12:27:43 +0100

    add fluent API relations

--- Context.cs
+++ Context.cs
@@ -12,7 +12,7 @@ public class Context : DbContext
     public DbSet<InterestApp> InterestApps { get; set; } = null!;
     public DbSet<LiaPitch> LiaPitches { get; set; } = null!;
     public DbSet<Student> Students { get; set; } = null!;
-    public DbSet<StudyProgram> Programs { get; set; } = null!;
+    public DbSet<StudyProgram> StudyPrograms { get; set; } = null!;
     public DbSet<Class> Classes { get; set; } = null!;
     public DbSet<Employment> Employments { get; set; } = null!;
 
@@ -23,6 +23,73 @@ public class Context : DbContext
 
      protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        
+        modelBuilder.Entity<Company>(entity =>
+        {
+            entity.Property(e => e.LastUpdated).HasDefaultValueSql("CURRENT_DATE");
+
+            entity.HasOne(e => e.Location)
+                  .WithMany(l => l.Companies);
+        });
+
+        modelBuilder.Entity<Location>(entity =>
+        {
+            entity.Property(e => e.Name).IsRequired().HasMaxLength(255);
+        });
+
+        modelBuilder.Entity<ContactPerson>(entity =>
+        {
+            entity.HasOne(e => e.Company)
+                    .WithMany(c => c.ContactPersons);
+        });
+
+        modelBuilder.Entity<ContactDetail>(entity =>
+        {
+            entity.HasOne(e => e.ContactPerson)
+                    .WithMany(c => c.ContactDetails);
+        });
+
+        modelBuilder.Entity<InterestApp>(entity =>
+        {
+            entity.HasOne(e => e.Company)
+                    .WithMany(c => c.InterestApplications);
+        });
+
+        modelBuilder.Entity<LiaPitch>(entity =>
+        {
+            entity.HasOne(e => e.Company)
+                    .WithMany(c => c.LIAPitches);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+
+            entity.HasOne(e => e.StudyProgram)
+                    .WithMany(p => p.Students);
+
+            entity.HasOne(e => e.Class)
+                    .WithMany(c => c.Students);
+        });
+
+        modelBuilder.Entity<StudyProgram>(entity =>
+        {
+            // Students defined already in Students
+            entity.HasMany(e => e.Classes)
+                .WithOne(c => c.StudyProgram);
+        });
+
+        modelBuilder.Entity<Class>(entity =>
+        {
+            //Students defined
+            //StudyProgram defined
+        });
+
+        modelBuilder.Entity<Employment>(entity =>
+        {
+            entity.HasOne(e => e.Student)
+                    .WithMany(s => s.Employments);
+
+            entity.HasOne(e => e.Company)
+                    .WithMany(c => c.Employments);
+        });
     }
 }
