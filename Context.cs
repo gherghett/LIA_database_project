42237a4816bb48505eabbb6a926f1015e285da54
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-09 12:37:42 +0100

    add seed data to Context

--- Context.cs
+++ Context.cs
@@ -91,5 +91,67 @@ public class Context : DbContext
             entity.HasOne(e => e.Company)
                     .WithMany(c => c.Employments);
         });
+
+        // ****************** Seed data *************
+
+        // Seed data for Locations
+        modelBuilder.Entity<Location>().HasData(
+            new Location { Id = 1, Name = "Borås" },
+            new Location { Id = 2, Name = "Göteborg" },
+            new Location { Id = 3, Name = "Ulricehamn" }
+        );
+
+        // Seed data for Companies
+        modelBuilder.Entity<Company>().HasData(
+            new Company { Id = 1, Name = "NetOnNet", Url = "https://netonnet.com", LocationId = 1 },
+            new Company { Id = 2, Name = "BitAddict", Url = "https://bitaddict.se", LocationId = 2 },
+            new Company { Id = 3, Name = "Advacy/Imerge", Url = "https://www.advacy.se", LocationId = 3 }
+        );
+
+        // Seed data for Contact Persons
+        modelBuilder.Entity<ContactPerson>().HasData(
+            new ContactPerson { Id = 1, Name = "Daniel Theliander", Position = "VD", Ranking = 1, CompanyId = 1 },
+            new ContactPerson { Id = 2, Name = "Helena Bragée", Position = "CEO", Ranking = 1, CompanyId = 2 },
+            new ContactPerson { Id = 3, Name = "Roland Svensson", Position = "VD", Ranking = 1, CompanyId = 3 }
+        );
+
+        // Seed data for Interest Applications
+        modelBuilder.Entity<InterestApp>().HasData(
+            new InterestApp { Id = 1, CompanyId = 1, Year = "2024" },
+            new InterestApp { Id = 2, CompanyId = 2, Year = "2023" },
+            new InterestApp { Id = 3, CompanyId = 3, Year = "2024" }
+        );
+
+        // Seed data for LIA Pitches
+        modelBuilder.Entity<LiaPitch>().HasData(
+            new LiaPitch { Id = 1, CompanyId = 1, Year = "2024", HasOccurred = false },
+            new LiaPitch { Id = 2, CompanyId = 2, Year = "2023", HasOccurred = true },
+            new LiaPitch { Id = 3, CompanyId = 3, Year = "2024", HasOccurred = false }
+        );
+
+        // Seed data for Programs
+        modelBuilder.Entity<StudyProgram>().HasData(
+            new StudyProgram { Id = 1, Name = "SUVNET" },
+            new StudyProgram { Id = 2, Name = "FRONTEND" }
+        );
+
+        // Seed data for Classes
+        modelBuilder.Entity<Class>().HasData(
+            new Class { Id = 1, Name = "SUVNET21", Year = 2021, StudyProgramId = 1 },
+            new Class { Id = 2, Name = "FRONTEND22", Year = 2022, StudyProgramId = 2 }
+        );
+
+        // Seed data for Students
+        modelBuilder.Entity<Student>().HasData(
+            new Student { Id = 1, Name = "Alice Andersson", StudyProgramId = 1, ClassId = 1 },
+            new Student { Id = 2, Name = "Bob Bengtsson", StudyProgramId = 2, ClassId = 2 }
+        );
+
+        // Seed data for Employment
+        modelBuilder.Entity<Employment>().HasData(
+            new Employment { Id = 1, StudentId = 1, CompanyId = 1, EmploymentDate = new DateTime(2024, 01, 15) },
+            new Employment { Id = 2, StudentId = 2, CompanyId = 2, EmploymentDate = new DateTime(2023, 11, 30) }
+        );
+
     }
 }
