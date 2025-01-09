29924b4a13dd620a9a6767d03c29b6cfb8ad2eaf
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-09 12:09:13 +0100

    add DbSets to context

--- Context.cs
+++ Context.cs
@@ -6,6 +6,15 @@ using Models.Entities;
 public class Context : DbContext
 {
     public DbSet<Company> Companies { get; set; } = null!;
+    public DbSet<Location> Locations { get; set; } = null!;
+    public DbSet<ContactPerson> ContactPersons { get; set; } = null!;
+    public DbSet<ContactDetail> ContactDetails { get; set; } = null!;
+    public DbSet<InterestApp> InterestApps { get; set; } = null!;
+    public DbSet<LiaPitch> LiaPitches { get; set; } = null!;
+    public DbSet<Student> Students { get; set; } = null!;
+    public DbSet<StudyProgram> Programs { get; set; } = null!;
+    public DbSet<Class> Classes { get; set; } = null!;
+    public DbSet<Employment> Employments { get; set; } = null!;
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
