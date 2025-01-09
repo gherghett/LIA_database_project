3465d33c9a11c1e510b32a2ee0ac0246747ad0d8
Daniel Gherghetta <gherghetta@gmail.com>
2025-01-09 12:59:11 +0100

    run and apply migrations

---
+++ Migrations/20250109114019_InitialCreate.cs
@@ -0,0 +1,393 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LIA.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Locations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 255, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Locations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StudyPrograms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StudyPrograms", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Url = table.Column<string>(type: "TEXT", nullable: true),
+                    LastUpdated = table.Column<DateTime>(type: "TEXT", nullable: false, defaultValueSql: "CURRENT_DATE"),
+                    LocationId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Companies_Locations_LocationId",
+                        column: x => x.LocationId,
+                        principalTable: "Locations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Classes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    StudyProgramId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Classes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Classes_StudyPrograms_StudyProgramId",
+                        column: x => x.StudyProgramId,
+                        principalTable: "StudyPrograms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactPersons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Ranking = table.Column<int>(type: "INTEGER", nullable: false),
+                    Position = table.Column<string>(type: "TEXT", nullable: false),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactPersons", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactPersons_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InterestApps",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Year = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InterestApps", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InterestApps_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LiaPitches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Year = table.Column<string>(type: "TEXT", nullable: false),
+                    HasOccurred = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LiaPitches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_LiaPitches_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    StudyProgramId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ClassId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Classes_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Classes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Students_StudyPrograms_StudyProgramId",
+                        column: x => x.StudyProgramId,
+                        principalTable: "StudyPrograms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ContactDetails",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ContactPersonId = table.Column<int>(type: "INTEGER", nullable: false),
+                    ContactInfo = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ContactDetails", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ContactDetails_ContactPersons_ContactPersonId",
+                        column: x => x.ContactPersonId,
+                        principalTable: "ContactPersons",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Employments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    StudentId = table.Column<int>(type: "INTEGER", nullable: false),
+                    CompanyId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EmploymentDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Employments_Companies_CompanyId",
+                        column: x => x.CompanyId,
+                        principalTable: "Companies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Employments_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Locations",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Borås" },
+                    { 2, "Göteborg" },
+                    { 3, "Ulricehamn" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "StudyPrograms",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "SUVNET" },
+                    { 2, "FRONTEND" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Classes",
+                columns: new[] { "Id", "Name", "StudyProgramId", "Year" },
+                values: new object[,]
+                {
+                    { 1, "SUVNET21", 1, 2021 },
+                    { 2, "FRONTEND22", 2, 2022 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "LocationId", "Name", "Url" },
+                values: new object[,]
+                {
+                    { 1, 1, "NetOnNet", "https://netonnet.com" },
+                    { 2, 2, "BitAddict", "https://bitaddict.se" },
+                    { 3, 3, "Advacy/Imerge", "https://www.advacy.se" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ContactPersons",
+                columns: new[] { "Id", "CompanyId", "Name", "Position", "Ranking" },
+                values: new object[,]
+                {
+                    { 1, 1, "Daniel Theliander", "VD", 1 },
+                    { 2, 2, "Helena Bragée", "CEO", 1 },
+                    { 3, 3, "Roland Svensson", "VD", 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "InterestApps",
+                columns: new[] { "Id", "CompanyId", "Year" },
+                values: new object[,]
+                {
+                    { 1, 1, "2024" },
+                    { 2, 2, "2023" },
+                    { 3, 3, "2024" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "LiaPitches",
+                columns: new[] { "Id", "CompanyId", "HasOccurred", "Year" },
+                values: new object[,]
+                {
+                    { 1, 1, false, "2024" },
+                    { 2, 2, true, "2023" },
+                    { 3, 3, false, "2024" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Students",
+                columns: new[] { "Id", "ClassId", "Name", "StudyProgramId" },
+                values: new object[,]
+                {
+                    { 1, 1, "Alice Andersson", 1 },
+                    { 2, 2, "Bob Bengtsson", 2 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Employments",
+                columns: new[] { "Id", "CompanyId", "EmploymentDate", "StudentId" },
+                values: new object[,]
+                {
+                    { 1, 1, new DateTime(2024, 1, 15, 0, 0, 0, 0, DateTimeKind.Unspecified), 1 },
+                    { 2, 2, new DateTime(2023, 11, 30, 0, 0, 0, 0, DateTimeKind.Unspecified), 2 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Classes_StudyProgramId",
+                table: "Classes",
+                column: "StudyProgramId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Companies_LocationId",
+                table: "Companies",
+                column: "LocationId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactDetails_ContactPersonId",
+                table: "ContactDetails",
+                column: "ContactPersonId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ContactPersons_CompanyId",
+                table: "ContactPersons",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employments_CompanyId",
+                table: "Employments",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employments_StudentId",
+                table: "Employments",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InterestApps_CompanyId",
+                table: "InterestApps",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LiaPitches_CompanyId",
+                table: "LiaPitches",
+                column: "CompanyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_ClassId",
+                table: "Students",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_StudyProgramId",
+                table: "Students",
+                column: "StudyProgramId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ContactDetails");
+
+            migrationBuilder.DropTable(
+                name: "Employments");
+
+            migrationBuilder.DropTable(
+                name: "InterestApps");
+
+            migrationBuilder.DropTable(
+                name: "LiaPitches");
+
+            migrationBuilder.DropTable(
+                name: "ContactPersons");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Companies");
+
+            migrationBuilder.DropTable(
+                name: "Classes");
+
+            migrationBuilder.DropTable(
+                name: "Locations");
+
+            migrationBuilder.DropTable(
+                name: "StudyPrograms");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Models.Entities;
+
+Console.WriteLine("Hello, Krister!");
+
+
+using (var context = new Context())
+{
+    var count = context.Companies.Count();
+    Console.WriteLine($"Number of companies: {count}");
+}
+
